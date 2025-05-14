96067896c4257dcd3a97806dca7343576f7d8820
rajesh chaudhari <93049758+rajeshchau@users.noreply.github.com>
2025-05-14 13:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function mapLetters(str) {
+  let newobj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (!newobj[char]) {
+      newobj[char] = [];
+    }
+    newobj[char].push(i);
+  }
+
+  return newobj;
+}
+
+console.log(mapLetters("hello"));
